6efeab52fe66f71f78722a190f093eb76fdcf622
rafec <rafael.brizuena@gmail.com>
2024-02-06 21:58:41 -0400

    created `ormconfig.json` file and created first migration using `typeorm`

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,8 @@
+import "reflect-metadata";
 import express from "express";
 
+import "./database";
+
 const app = express();
-
-app.get("/test", (request, response) => {
-	return response.send("Hello, NLW!");
-});
-
-app.post("/test-post", (request, response) => {
-	return response.send("Hello, NLW! Using POST.");
-});
 
 app.listen(3000, () => console.log("Server is running"));
