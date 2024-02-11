df724ad172fa17c6ab6a7a5bbba17027dbb0632f
rafec <rafael.brizuena@gmail.com>
2024-02-11 12:33:41 -0400

    created `ensureAuthenticated.ts` middleware to validate the token and extract user info. Also added custom types folder for `Request` from `Express`

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -1,12 +1,33 @@
 import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+
+interface IPayload {
+	sub: string;
+}
 
 export function ensureAuthenticated(
 	request: Request,
 	response: Response,
 	next: NextFunction
 ) {
-	const token = request.headers.authorization;
-	console.log(token);
+	const authToken = request.headers.authorization;
 
-	return next();
+	if (!authToken) {
+		return response.status(401).end();
+	}
+
+	const [, token] = authToken.split(" ");
+
+	try {
+		const { sub } = verify(
+			token,
+			"2bc55ba6bedc8abd7a5bd8e6262fbd84"
+		) as IPayload;
+
+		request.user_id = sub;
+
+		return next();
+	} catch (err) {
+		return response.status(401).end();
+	}
 }
